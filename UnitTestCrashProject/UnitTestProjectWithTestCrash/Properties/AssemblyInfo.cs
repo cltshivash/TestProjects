8111980f429ac2c60789ee3803a7d5522a536f5e
Shiva Shankar (VSTT) <shivash@microsoft.com>
2018-05-14 01:31:25 -0700

    Intiial checkin of the project with a sample UT

---
+++ UnitTestCrashProject/UnitTestProjectWithTestCrash/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectWithTestCrash")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectWithTestCrash")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8e9d9b0-fa8b-4fb1-98a4-7b4f98820bfa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
