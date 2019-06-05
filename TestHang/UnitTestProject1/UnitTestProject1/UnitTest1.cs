44c3fd760edead941582772b501bd6c327a6c2c9
Shiva Shankar Thangadurai <shivash@microsoft.com>
2019-06-05 19:39:43 +0530

    Added a TestHang solution

---
+++ TestHang/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Thread.Sleep(20 * 60 * 1000);
+        }
+    }
+}
