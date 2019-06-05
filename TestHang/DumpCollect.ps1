a84cf6eb6e7e0e2a5773f201ae566929a62529af
Shiva Shankar Thangadurai <shivash@microsoft.com>
2019-06-05 21:33:39 +0530

    Minor fixes

--- TestHang/DumpCollect.ps1
+++ TestHang/DumpCollect.ps1
@@ -22,30 +22,33 @@ try
 	if (!(Test-Path $Destination)) {
 		$null = New-Item -Path $Destination -ItemType Directory
 	}
+	
+	# Create the log path
 	$log = Join-Path $Destination ('dmp_{0}.log' -f [Guid]::NewGuid())
 	
-	# Do not block the current process unless -Wait is passed.
-	if (!$Wait) {
+	# File that serves to indicate whether to wait or not..
+	$waitEnabledPath = Join-Path $Destination '.waitfortargetprocess'
+	
+	if (![System.IO.File]::Exists($waitEnabledPath)) {
+		
+		Write-Host "Wait for process"
+		"Wait for process" | Out-File $waitEnabledPath
 		
 		[string[]] $params = @('-File') + $MyInvocation.MyCommand.Path
-		$params += '-Wait'
 
-		Write-Host "Invoking with Wait..."
-		"Invoking with Wait..." | Out-File $log
 		Write-Host "TEMP Location :" $Destination
-		Write-Host $params
+		Write-Host "Parameters for the invocation : " $params
+
 		Start-Process -FilePath "${env:WINDIR}\System32\WindowsPowerShell\v1.0\powershell.exe" -ArgumentList $params
-		Start-Sleep -Milliseconds 10000
 		Write-Host "Ending..."
 		return
 	}
 	
-	"Waiting for $Timeout before creating dumps for process(es): $ProcessName" | Out-File $log
+	"Waiting for $Timeout before creating dumps for process(es): $ProcessName" | Out-File $log -Append
+	Write-Host "Waiting for $Timeout before creating dumps for process(es): $ProcessName"
 	Start-Sleep -Milliseconds $Timeout.TotalMilliseconds
-
-	$log = Join-Path $Destination ('dmp_{0}.log' -f [Guid]::NewGuid())
+	
 	"Checking for process(es): $ProcessName" | Out-File $log
-
 	Get-Process -Name $ProcessName | ForEach-Object {
 		$path = Join-Path $Destination ('{0}_{1}.dmp' -f $_.Name, $_.Id)
 		"Writing dump for process: $($_.Name), path: $path" | Out-File $log -Append
@@ -66,7 +69,10 @@ try
 catch {
     # Write error and continue processing
 	"Error: Failed to write dump: $path, error: $_" | Out-File $log -Append
+	
 }
 
-"Error: Failed to write dump: $path, error: $_" | Out-File $log -Append
+if ([System.IO.File]::Exists($waitEnabledPath)) {
+	[System.IO.File]::Delete($waitEnabledPath)
+}
 
