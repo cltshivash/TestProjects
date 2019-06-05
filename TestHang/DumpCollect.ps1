2b61104323768c1b5a3128b0a3df5ec53b97a1bb
Shiva Shankar Thangadurai <shivash@microsoft.com>
2019-06-05 20:07:15 +0530

    Added script for tacking dumps

--- TestHang/DumpCollect.ps1
+++ TestHang/DumpCollect.ps1
@@ -6,7 +6,7 @@ param (
 
     [Parameter()]
     [ValidateNotNullOrEmpty()]
-    [string] $Destination = (join-path $env:TEMP ([Guid]::NewGuid().ToString('n'))),
+    [string] $Destination = (join-path $env:TEMP "TestHangDumps"),
 
     [Parameter()]
     [ValidateNotNullOrEmpty()]
@@ -23,9 +23,46 @@ if (!$Wait) {
     $params += '-Wait'
 
     Write-Host "Invoking with Wait..."
+	Write-Host "TEMP Location :" $Destination
     Write-Host $params
     Start-Process -FilePath "${env:WINDIR}\System32\WindowsPowerShell\v1.0\powershell.exe" -ArgumentList $params
     return
 }
 
-Write-Host "Actual Process"
+try
+{
+	if (!(Test-Path $Destination)) {
+		$null = New-Item -Path $Destination -ItemType Directory
+	}
+
+	$log = Join-Path $Destination ('dmp_{0}.log' -f [Guid]::NewGuid())
+	"Waiting for $Timeout before creating dumps for process(es): $ProcessName" | Out-File $log
+	Start-Sleep -Milliseconds $Timeout.TotalMilliseconds
+
+	$log = Join-Path $Destination ('dmp_{0}.log' -f [Guid]::NewGuid())
+	"Checking for process(es): $ProcessName" | Out-File $log
+
+	Get-Process -Name $ProcessName | ForEach-Object {
+		$path = Join-Path $Destination ('{0}_{1}.dmp' -f $_.Name, $_.Id)
+		"Writing dump for process: $($_.Name), path: $path" | Out-File $log -Append
+
+		try {
+			
+			procdump -s 5 -n 2 -accepteula $_.Id $Destination
+			"Successfully wrote dump: $path" | Out-File $log -Append
+
+			# Attempt to kill the process to free any file locks.
+			$_ | Stop-Process
+		}
+		catch {
+			"Error: Failed to write dump: $path, error: $_" | Out-File $log -Append
+		}
+	}
+}
+catch {
+    # Write error and continue processing
+	"Error: Failed to write dump: $path, error: $_" | Out-File $log -Append
+}
+
+"Error: Failed to write dump: $path, error: $_" | Out-File $log -Append
+
